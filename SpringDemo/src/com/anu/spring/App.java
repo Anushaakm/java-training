e42ea14eedd23b9b1b05142c8bcf38f64a5e7e79
Anushaakm <anushakm.xworkz@gmail.com>
2023-04-03 12:28:39 +0530

    spring

--- SpringDemo/src/com/anu/spring/App.java
+++ SpringDemo/src/com/anu/spring/App.java
@@ -2,28 +2,47 @@ package com.anu.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.anu.spring.config.AppConfig;
 import com.anu.spring.util.FileChecker;
 import com.anu.spring.util.FileUtil;
 
-public class App {
 
-	  public static void normal() {
-	        FileUtil util = new FileUtil();
 
-	        FileChecker checker = new FileChecker(util);
-	        checker.checkFileValidity();
-	    }
+public final class App {
 
-	    public static void withSpring() {
-	        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void normal() {
+        System.out.println("Providing Dependencies Normally");
 
-	        FileChecker checker = ctx.getBean(FileChecker.class);
-	        checker.checkFileValidity();
-	    }
+        FileUtil util = new FileUtil();
 
-	    public static void main(String[] args) {
-	        withSpring();
-	    }
+        FileChecker checker = new FileChecker(util);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringJavaConfig() {
+        System.out.println("Providing Dependencies With Java Code");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] names = ctx.getBeanDefinitionNames();
+        for(String nm : names) {
+            System.out.println(nm);
+        }
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringXmlConfig() {
+        System.out.println("Providing Dependencies With Xml Config");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void main(String[] args) {
+        withSpringXmlConfig();
+    }
 }
